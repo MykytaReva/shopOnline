06ff8ba088a91656ebab29afaebc3cc941298145
Mykyta Reva <mykytareva@gmail.com>
2023-04-04 17:35:56 +0200

    trans ajax to js file

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -19,3 +19,36 @@ function checkRole(answer) {
 
 }
 
+$(document).ready(function() {
+  // submit form data using AJAX
+  $('#newsletter-form').on('submit', function(event) {
+    event.preventDefault();
+
+    // disable submit button
+    $('#report-btn').attr('disabled', true);
+
+    // send AJAX request
+    $.ajax({
+      url: '/accounts/newsletter/', // replace with actual URL path
+      type: 'POST',
+      data: $(this).serialize(),
+      success: function(response) {
+        // enable submit button
+        $('#report-btn').attr('disabled', false);
+
+        // clear email input field
+        $('#daily-email-id').val('');
+
+        // show success/error message
+        Swal.fire({
+          icon: response.icon,
+          title: response.message,
+        });
+      },
+      error: function(xhr, status, error) {
+        // enable submit button
+        $('#report-btn').attr('disabled', false);
+      }
+    });
+  });
+});
